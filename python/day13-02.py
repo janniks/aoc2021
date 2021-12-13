734b54d77acb717ec4d523e1199a5d22ba217183
janniks <jannik.siebert@gmail.com>
2021-12-13 23:32:59 +0100

    Remove unnecessary python

--- python/day13-02.py
+++ python/day13-02.py
@@ -7,7 +7,7 @@ import sys
 from collections import namedtuple
 from functools import partial
 from itertools import takewhile
-from operator import attrgetter, ne
+from operator import ne
 
 import numpy as np
 
@@ -17,24 +17,13 @@ input_dots = list(map(lambda s: list(map(int, s.split(','))), takewhile(partial(
 input_folds = list(map(lambda s: Fold(*s.replace('fold along ', '').split('=')), map(str.strip, sys.stdin)))
 
 xs, ys = zip(*input_dots)
-
 paper = np.zeros((max(ys)+1, max(xs)+1), dtype=int)
 paper[ys, xs] = 1
 
 def origami(paper, fold):
     matrix = paper.view() if fold.axis == 'y' else np.transpose(paper)
-
     v = int(fold.value)
-    keep = matrix[:v, :].copy()
-    cut = np.flipud(matrix[v+1:, :]).copy()
-
-    smaller, larger = sorted([keep, cut], key=attrgetter('shape'))
-    diff = larger.shape[0] - smaller.shape[0]
-    # pad the smaller with zeroes on top
-    smaller = np.pad(smaller, [(diff, 0), (0,0)])
-
-    matrix = larger + smaller
-
+    matrix = matrix[:v, :] + np.flipud(matrix[v+1:, :])
     return matrix if fold.axis == 'y' else np.transpose(matrix)
 
 for fold in input_folds:
